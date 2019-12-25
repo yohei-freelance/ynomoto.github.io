eb77e6891763ed569b36d014a0a4f7c63676bd8a
yohei-freelance <n-yohei-1717@g.ecc.u-tokyo.ac.jp>
2019-12-26 00:54:07 +0900

    commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+   $('.nice-textbox').blur(function() {
+        if($(this).val().length === 0){
+          $('.nice-label').removeClass("focus");
+        }
+        else { returns; }
+      })
+      .focus(function() {
+        $('.nice-label').addClass("focus")
+      });
+});
